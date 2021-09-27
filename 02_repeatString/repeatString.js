b8700759bc3cd9abb8e9a3ff05caaebeea6c73dc
raylu15742cs <raylu15742cs@gmail.com>
2021-09-27 15:43:38 -0700

    new-message

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    i = 1;
+    newstring = "";
+    if (num >= 0){
+        while ( i <= num) {
+            newstring += string;
+            i += 1;
+        }
+        return newstring;
+    } else {
+        error = "ERROR";
+        return error;
+    }
 };
 
 // Do not edit below this line
