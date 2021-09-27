6d7894c3a5807a3909870d0702e6237512076514
raylu15742cs <raylu15742cs@gmail.com>
2021-09-27 16:23:36 -0700

    new-message

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function(str) {
-
-};
+    leng = str.length;
+    let i = leng;
+    reverse = "";
+    while (i >= 1) {
+        reverse += str[i-1];
+        i--;
+    }
+    return reverse;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
