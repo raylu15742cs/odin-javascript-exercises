53349c28996fd924a241dd17a1ea713bc1d98029
raylu15742cs <raylu15742cs@gmail.com>
2021-09-28 13:09:18 -0700

    new-message

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  temp = 0;
+  temp = (deg - 32) * (5/9);
+  temp = Math.round(temp*10) / 10;
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  temp = 0;
+  temp = (deg * 1.8) + 32;
+  temp = Math.round(temp*10) / 10;
+  return temp;
 };
 
 // Do not edit below this line
