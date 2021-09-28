fd55f2061b619d8cfc320f0513ecbccba5006acb
raylu15742cs <raylu15742cs@gmail.com>
2021-09-28 12:43:30 -0700

    new-message

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, first, second, third, fourth) {
+    code = "";
+    const removeValue = function(array1, test) {
+        let i = 0;
+        while (i <= array1.length) {
+            if (array1[i] === test){
+                array1.splice(i,1);
+                i += 1;
+            } else {
+                i += 1;
+            }
+        }
+    }
+    if (first != undefined) {
+        removeValue(array1, first);
+    }
+    if (second != undefined) {
+        removeValue(array1, second);
+    }
+    if (third != undefined) {
+        removeValue(array1, third);
+    }
+    if (fourth != undefined) {
+        removeValue(array1, fourth);
+    }
 
+    return (array1);
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    sum = 0;
+    if (typeof one === "number" && typeof two === "number"){
+        if (one >= 0 && two >= 0){
+            if (one < two) {
+                while (one <= two) {
+                    sum += one;
+                    one += 1;
+                }
+                return sum;
+            } else {
+                while (one >= two) {
+                    sum += one;
+                    one -= 1;
+                }
+                return sum;
 
+            }
+        } else {
+            return ("ERROR")
+        }
+    } else {
+        return ("ERROR")
+    }
 };
 
 // Do not edit below this line
