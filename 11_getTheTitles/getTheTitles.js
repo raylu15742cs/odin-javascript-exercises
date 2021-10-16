a2cd013c817e92bfa02a9b85c68a523196d34246
raylu15742cs <raylu15742cs@gmail.com>
2021-10-16 03:46:22 -0700

    new-message

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(value) {
+    let i = 0;
+    let result = [];
+    while (value.length > i) {
+        result.push (value[i].title);
+        i += 1;
+    }
+    return result;
 };
 
 // Do not edit below this line
