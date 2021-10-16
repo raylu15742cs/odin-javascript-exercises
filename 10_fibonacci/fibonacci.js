0b150b3fe90fbd8f28f21597ab4b7704878fc743
raylu15742cs <raylu15742cs@gmail.com>
2021-10-16 03:13:38 -0700

    new message

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+ let nums = [];
+ let i = 0;
+if (fib >= 0 ){
+    while ( i < fib) {
+        if ( i < 2){
+            nums.push(1);
+        } else {
+            nums.push(nums[i - 1] + nums[i - 2]);
+        }         
+        i ++;
+    }
+} else {
+    return ("OOPS");
+}
 
+ return nums[fib - 1];
 };
 
 // Do not edit below this line
