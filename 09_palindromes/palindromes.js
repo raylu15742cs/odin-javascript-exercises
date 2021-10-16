414698ec9058dbd2c760e378e9913a69d0563ee3
raylu15742cs <raylu15742cs@gmail.com>
2021-10-16 02:13:07 -0700

    new message

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let leng = string.length;
+    let news = '';
+    let testnews = '';
+    let counter = 0;
+    while (leng > 0) {
+        if ((/[a-zA-z]/).test(string[counter])) {
+            news += string[counter];
+        }
+    counter += 1;
+    leng -= 1;
+    }
+    news = news.toLowerCase();
+    let newsl = news.length;
+    while (newsl > 0) {
+        testnews += news[newsl - 1];
+        newsl --;
+    }
+    if(testnews === news) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
